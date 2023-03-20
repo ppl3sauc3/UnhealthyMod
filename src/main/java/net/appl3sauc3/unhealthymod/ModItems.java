410f2856df6bc413e0383814665790aab93eb6c6
ppl3sauc3 <51028391+ppl3sauc3@users.noreply.github.com>
2023-03-20 14:36:10 -0400

    Mod items

---
+++ src/main/java/net/appl3sauc3/unhealthymod/ModItems.java
@@ -0,0 +1,20 @@
+package com.appl3sauc3.unhealthymod.item;
+
+import com.appl3sauc3.unhealthymod.unhealthymod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, unhealthymod.MODID);
+
+    public static final RegistryObject<Item> BLACK_OPAL = ITEMS.register("black_opal"
+    () -> new Item(new Item.Properties()))
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
